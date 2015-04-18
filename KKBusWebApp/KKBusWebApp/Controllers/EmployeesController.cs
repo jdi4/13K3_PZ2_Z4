39bee87e9c7b435d180985dd8dc8c73ec15b5cfa
jdi4 <jzxp.a4t@gmail.com>
2015-04-18 16:52:46 +0200

    Pliki projektu bez niepotrzebnych plików
    To co najlepsze ze starej gałęzi mvc_test1 :smiley:

---
+++ KKBusWebApp/KKBusWebApp/Controllers/EmployeesController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+// wbrew nazwie to jest tylko testowy kontroler - niekoniecznie coś dalej z niego będzie
+
+namespace KKBusWebApp.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        //
+        // GET: /Employees/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewBag.Message = "Hello " + name;
+            ViewBag.NumTimes = numTimes;
+
+            return View();
+        }
+
+        //public string Welcome(string name, int ID = 1)
+        //{
+        //    return HttpUtility.HtmlEncode("Hello " + name + ", ID: " + ID);
+        //}
+	}
+}
