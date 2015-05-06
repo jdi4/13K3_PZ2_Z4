02cad477322de8d6a8e1859ba20dd0406e899b10
jogi32 <tomasz.szafranski32@gmail.com>
2015-05-06 21:14:10 +0200

    Basic TDD
    Basic TDD build with primary test and config.
    Waiting on other branches to be merged

---
+++ KKBusWebApp/TDD/unittest1.cpp
@@ -0,0 +1,18 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+#include "..\KKBusWebApp\Models\AccountViewModels.cs"
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace TDD
+{		
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(ExternalLoginConfirmationViewModel)
+		{
+			
+		}
+
+	};
+}
