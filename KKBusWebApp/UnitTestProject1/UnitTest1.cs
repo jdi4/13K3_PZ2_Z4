6bd19ec5f8f1c933cbd3948786ac2347db203381
jogi32 <tomasz.szafranski32@gmail.com>
2015-05-06 21:14:10 +0200

    Basic TDD
    Basic TDD build with primary test and config.
    Waiting on other branches to be merged

---
+++ KKBusWebApp/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+//using KKBusWebApp.Models;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void ExternalLoginConfirmationViewModel()
+        {
+        }
+        public void ManageUserViewModel()
+        {
+        }
+        public void LoginViewModel()
+        {
+        }
+        public void RegisterViewModel()
+        {
+        }
+    }
+}
