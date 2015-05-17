0612e7a73bf11a4c53ccc739df0bc84c077aa0d1
jdi4 <jzxp.a4t@gmail.com>
2015-05-17 10:38:07 +0200

    Some modifications to our custom Identity Model
    Stopped using ApplicationUser for now - OSOBY fully represents users.
    Changed used interfaces for UserStore to only most needed and
    implemented IQueryableUserStore<OSOBY, int>

--- KKBusWebApp/KKBusWebApp/Models/IdentityModels.cs
+++ KKBusWebApp/KKBusWebApp/Models/IdentityModels.cs
@@ -1,13 +1,17 @@
 ﻿using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Globalization;
+using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace KKBusWebApp.Models
 {
     public partial class OSOBY : IUser<int>
     {
-
         public int Id
         {
             get { return this.OSO_ID; }
@@ -24,13 +28,31 @@ namespace KKBusWebApp.Models
                 this.OSO_LOGIN = value;
             }
         }
+
+        public string PasswordHash
+        {
+            get
+            {
+                return this.OSO_HASLO;
+            }
+            set
+            {
+                this.OSO_HASLO = value;
+            }
+        }
+    }
+
+    // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
+    public class ApplicationUser : OSOBY
+    {
+
     }
 
     public partial class UserStore : 
-        IUserRoleStore<ApplicationUser, int>, IQueryableUserStore<ApplicationUser, int>, IUserPasswordStore<ApplicationUser, int>, 
-        IUserLoginStore<ApplicationUser, int>, IUserClaimStore<ApplicationUser, int>, IUserSecurityStampStore<ApplicationUser, int>,
-        IUserEmailStore<ApplicationUser, int>, IUserPhoneNumberStore<ApplicationUser, int>, IUserTwoFactorStore<ApplicationUser, int>,
-        IUserLockoutStore<ApplicationUser, int>
+        IQueryableUserStore<OSOBY, int>, IUserPasswordStore<OSOBY, int>,
+        IUserRoleStore<OSOBY, int>,
+        IUserEmailStore<OSOBY, int>, IUserPhoneNumberStore<OSOBY, int>
+
     {
         private readonly sql372873Entities db;
 
@@ -45,18 +67,165 @@ namespace KKBusWebApp.Models
             
         }
 
-    }
-
-    // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
-    public class ApplicationUser : OSOBY
-    {
-    }
-
-    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
-    {
-        public ApplicationDbContext()
-            : base("kkbustestdbEntities")
+        public IQueryable<OSOBY> Users
         {
+            get { return this.db.OSOBY; }
+        }
+
+        public Task CreateAsync(OSOBY user)
+        {
+            this.db.OSOBY.Add(user);
+            return this.db.SaveChangesAsync();
+        }
+
+        public Task DeleteAsync(OSOBY user)
+        {
+            this.db.OSOBY.Remove(user);
+            return this.db.SaveChangesAsync();
+        }
+
+        public Task<OSOBY> FindByIdAsync(int userId)
+        {
+            return this.db.OSOBY.FirstOrDefaultAsync(u => u.Id.Equals(userId));
+        }
+
+        public Task<OSOBY> FindByNameAsync(string userName)
+        {
+            return this.db.OSOBY
+                .FirstOrDefaultAsync(u => u.UserName == userName);
+        }
+
+        public Task UpdateAsync(OSOBY user)
+        {
+            this.db.Entry<OSOBY>(user).State = EntityState.Modified;
+            return this.db.SaveChangesAsync();
+        }
+
+        public void Dispose()
+        {
+            this.Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing && this.db != null)
+            {
+                this.db.Dispose();
+            }
+        }
+
+        // IUserPasswordStore<OSOBY, int>
+
+        public Task<string> GetPasswordHashAsync(OSOBY user)
+        {
+            if (user == null)
+            {
+                throw new ArgumentNullException("user");
+            }
+
+            return Task.FromResult(user.PasswordHash);
+        }
+
+        public Task<bool> HasPasswordAsync(OSOBY user)
+        {
+            return Task.FromResult(user.PasswordHash != null);
+        }
+
+        public Task SetPasswordHashAsync(OSOBY user, string passwordHash)
+        {
+            if (user == null)
+            {
+                throw new ArgumentNullException("user");
+            }
+
+            user.PasswordHash = passwordHash;
+            return Task.FromResult(0);
+        }
+
+        // IUserRoleStore<OSOBY, int>
+
+        public Task AddToRoleAsync(OSOBY user, string roleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IList<string>> GetRolesAsync(OSOBY user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> IsInRoleAsync(OSOBY user, string roleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RemoveFromRoleAsync(OSOBY user, string roleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        // IUserEmailStore<OSOBY, int>
+
+        public Task<OSOBY> FindByEmailAsync(string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> GetEmailAsync(OSOBY user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> GetEmailConfirmedAsync(OSOBY user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SetEmailAsync(OSOBY user, string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SetEmailConfirmedAsync(OSOBY user, bool confirmed)
+        {
+            throw new NotImplementedException();
+        }
+
+        // IUserPhoneNumberStore<OSOBY, int>
+
+        public Task<string> GetPhoneNumberAsync(OSOBY user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> GetPhoneNumberConfirmedAsync(OSOBY user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SetPhoneNumberAsync(OSOBY user, string phoneNumber)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SetPhoneNumberConfirmedAsync(OSOBY user, bool confirmed)
+        {
+            throw new NotImplementedException();
         }
     }
+
+    public class ApplicationRoles : IdentityRole
+    {
+
+    }
+
+
+    //public class ApplicationDbContext : IdentityDbContext<OSOBY>
+    //{
+    //    public ApplicationDbContext()
+    //        : base("kkbustestdbEntities")
+    //    {
+    //    }
+    //}
 }
